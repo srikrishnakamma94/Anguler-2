a3619e6cf321665cb9e1f9a29957b4a8b79c5cd3
srikrishnakamma94 <srikrishnakamma@gmail.com>
2017-03-01 14:03:39 +0530

    added router functionallity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<user></user>`,
+  template: `
+              <ul>
+                <li>
+                  <a routerLink="/">  Home </a> 
+                </li>
+                <li>
+                  <a routerLink="/about">  about </a> 
+                </li>
+              </ul>
+              <router-outlet></router-outlet>
+            `,
 })
 export class AppComponent  {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { UserComponent }  from './components/user.component';
+import { AboutComponent }  from './components/about.component';
+import { routing }  from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpModule ],
-  declarations: [ AppComponent, UserComponent ],
+  imports:      [ BrowserModule, FormsModule, HttpModule, routing ],
+  declarations: [ AppComponent, UserComponent, AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
